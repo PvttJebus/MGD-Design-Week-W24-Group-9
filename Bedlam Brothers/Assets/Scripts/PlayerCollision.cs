6ca298966febac61c93af7bfdbe239e9c1efd3db
SethRodgerPatterson <000915695@mohawkcollege.ca>
2024-03-01 03:24:47 -0500

    collision detction1

---
+++ Bedlam Brothers/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag != "Tilemap_CollionWalls")
+        {
+            globalVariables.durability -= 1;
+        }
+        else
+        {
+            globalVariables.cleanliness -= 1;
+        }
+        
+        
+        Debug.Log("Collision!");
+
+    }
+
+}
